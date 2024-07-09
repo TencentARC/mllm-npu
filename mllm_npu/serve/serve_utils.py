d17391b497c9a667a95d62bcf79cafc527830ef6
palchenli <101778195+palchenli@users.noreply.github.com>
2024-07-09 21:02:22 +0800

    update

--- mllm_npu/serve/serve_utils.py
+++ mllm_npu/serve/serve_utils.py
@@ -8,6 +8,8 @@ import logging.handlers
 server_error_msg = "**NETWORK ERROR DUE TO HIGH TRAFFIC. PLEASE REGENERATE OR REFRESH THIS PAGE.**"
 moderation_msg = "YOUR INPUT VIOLATES OUR CONTENT MODERATION GUIDELINES. PLEASE TRY AGAIN."
 
+handler = None
+
 
 def pretty_print_semaphore(semaphore):
     if semaphore is None:
