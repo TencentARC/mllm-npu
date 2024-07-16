3d19459aa638f9219fce82b96e92dbdca964fc1a
palchenli <101778195+palchenli@users.noreply.github.com>
2024-07-16 13:24:40 +0800

    update

--- mllm_npu/evaluate/eval_data/bbh.py
+++ mllm_npu/evaluate/eval_data/bbh.py
@@ -29,8 +29,8 @@ def gen_prompt(train_data, subject, k=-1):
 def eval(model, tokenizer, subject, test_data, device):
     cors = []
 
-    for i in range(5, len(test_data["examples"])):
-    # for i in range(5, 10):
+    # for i in range(5, len(test_data["examples"])):
+    for i in range(5, 10):
         k = 5
         prompt_end = format_example(test_data, i, include_answer=False)
         train_prompt = gen_prompt(test_data, subject, k)
@@ -49,8 +49,8 @@ def eval(model, tokenizer, subject, test_data, device):
                 max_new_tokens=10
             )
 
-        # print("pred:", output['text'])
-        # print("label: ", label)
+        print("pred:", output['text'][:len(label)])
+        print("label: ", label)
         cor = output['text'][:len(label)] == label
         cors.append(cor)
 
